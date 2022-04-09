1077af0078620f4ca67106b7209048ea900cb42d
sjmyuan <shang.jiaming@163.com>
2022-04-09 23:05:35 +0800

    add option test

--- src/test/java/io/github/sjmyuan/vavr/OptionTest.java
+++ src/test/java/io/github/sjmyuan/vavr/OptionTest.java
@@ -2,13 +2,144 @@ package io.github.sjmyuan.vavr;
 
 import org.junit.Test;
 import io.vavr.control.Option;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;;
 
 public class OptionTest {
 
 
     @Test
-    void convertNullToNone() {
+    public void shouldConvertNullToNone() {
         Option<Integer> intValue = Option.of(null);
+        assertThat(intValue).isEqualTo(Option.none());
+    }
+
+    @Test
+    public void shouldConvertValueToSome() {
+        Option<Integer> intValue = Option.of(1);
+        assertThat(intValue).isEqualTo(Option.some(1));
+    }
+
+    @Test
+    public void shouldConvertOptionalToSomeIfPresent() {
+        Optional<Integer> optionalInt = Optional.of(1);
+        Option<Integer> intValue = Option.ofOptional(optionalInt);
+        assertThat(intValue).isEqualTo(Option.some(1));
+    }
+
+    @Test
+    public void shouldConvertOptionalToSomeIfNotPresent() {
+        Optional<Integer> optionalInt = Optional.ofNullable(null);
+        Option<Integer> intValue = Option.ofOptional(optionalInt);
+        assertThat(intValue).isEqualTo(Option.none());
+    }
+
+    @Test
+    public void canDoFilterForSome() {
+        Option<Integer> intValue = Option.of(1);
+        assertThat(intValue.filter(x -> x > 2)).isEqualTo(Option.none());
+        assertThat(intValue.filter(x -> x > 0)).isEqualTo(Option.some(1));
+    }
+
+    @Test
+    public void canDoFilterForNone() {
+        Option<Integer> intValue = Option.of(null);
+        assertThat(intValue.filter(x -> x > 2)).isEqualTo(Option.none());
+        assertThat(intValue.filter(x -> x <= 0)).isEqualTo(Option.none());
+    }
+
+    @Test
+    public void canDoMapForSome() {
+        Option<Integer> intValue = Option.of(1);
+        assertThat(intValue.map(x -> x + 1)).isEqualTo(Option.some(2));
+        assertThat(intValue.map(x -> x.toString())).isEqualTo(Option.some("1"));
+    }
+
+    @Test
+    public void canDoMapForNone() {
+        Option<Integer> intValue = Option.of(null);
+        assertThat(intValue.map(x -> x + 1)).isEqualTo(Option.none());
+        assertThat(intValue.map(x -> x.toString())).isEqualTo(Option.none());
+    }
+
+    @Test
+    public void canDoFlatMapForSome() {
+        Option<Integer> intValue = Option.of(1);
+        assertThat(intValue.flatMap(x -> Option.some(x + 1))).isEqualTo(Option.some(2));
+        assertThat(intValue.flatMap(x -> Option.none())).isEqualTo(Option.none());
+        assertThat(intValue.flatMap(x -> Option.some(x.toString()))).isEqualTo(Option.some("1"));
+    }
+
+    @Test
+    public void canDoFlatMapForNone() {
+        Option<Integer> intValue = Option.of(null);
+        assertThat(intValue.flatMap(x -> Option.some(x + 1))).isEqualTo(Option.none());
+        assertThat(intValue.flatMap(x -> Option.none())).isEqualTo(Option.none());
+        assertThat(intValue.flatMap(x -> Option.some(x.toString()))).isEqualTo(Option.none());
+    }
+
+    @Test
+    public void canDoPeekForSome() {
+        Option<Integer> intValue = Option.of(1);
+        List<Integer> intList = new ArrayList<Integer>();
+        intValue.peek(x -> {
+            intList.add(x);
+        });
+        assertThat(intList.size()).isEqualTo(1);
+        assertThat(intList.get(0)).isEqualTo(1);
+    }
+
+    @Test
+    public void canDoPeekForNone() {
+        Option<Integer> intValue = Option.of(null);
+        List<Integer> intList = new ArrayList<Integer>();
+        intValue.peek(x -> {
+            intList.add(x);
+        });
+        assertThat(intList.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void shouldReturnWrappedValueForSome() {
+        Option<Integer> intValue = Option.of(1);
+        assertThat(intValue.get()).isEqualTo(1);
+    }
+
+    @Test
+    public void shouldThrowErrorForNone() {
+        Option<Integer> intValue = Option.of(null);
+        assertThatThrownBy(() -> intValue.get()).isInstanceOf(NoSuchElementException.class)
+                .hasMessageContaining("No value present");
+    }
+
+    @Test
+    public void canReturnDefaultValueForNone() {
+        Option<Integer> intValue = Option.of(null);
+        assertThat(intValue.getOrElse(2)).isEqualTo(2);
+        assertThat(intValue.getOrElse(() -> 3)).isEqualTo(3);
+    }
+
+    @Test
+    public void canIgnoreDefaultValueForSome() {
+        Option<Integer> intValue = Option.of(1);
+        assertThat(intValue.getOrElse(2)).isEqualTo(1);
+        assertThat(intValue.getOrElse(() -> 3)).isEqualTo(1);
+    }
+
+    @Test
+    public void canThrowErrorForNone() {
+        Option<Integer> intValue = Option.of(null);
+        assertThatThrownBy(() -> intValue.getOrElseThrow(() -> new Error("empty value")))
+                .isInstanceOf(Error.class).hasMessageContaining("empty value");
+    }
+
+    @Test
+    public void canIgnoreThrowingErrorForSome() {
+        Option<Integer> intValue = Option.of(1);
+        assertThat(intValue.getOrElseThrow(() -> new Error("empty value"))).isEqualTo(1);
     }
 }
