68cb09f080ea77981794815b5b903258b3bbd3dc
sjmyuan <shang.jiaming@163.com>
2022-04-26 22:23:13 +0800

    add pattern test

--- src/test/java/io/github/sjmyuan/vavr/PatternTest.java
+++ src/test/java/io/github/sjmyuan/vavr/PatternTest.java
@@ -1,6 +1,7 @@
 package io.github.sjmyuan.vavr;
 
 import org.junit.Test;
+import io.vavr.control.Either;
 import io.vavr.control.Option;
 import static org.assertj.core.api.Assertions.assertThat;
 import static io.vavr.API.*;
@@ -11,18 +12,30 @@ public class PatternTest {
     @Test
     public void canMatchOption() {
 
-        // Integer result = Match(Option.some(1)).of(Case($Some($()), 1), Case($None(), 2));
+        Integer result =
+                Match(Option.some(1)).of(Case($Some($(x -> x < 2)), x -> x + 10), Case($None(), 2));
 
-        // assertThat(result).isEqualTo(1);
+        assertThat(result).isEqualTo(11);
+
+    }
+
+    @Test
+    public void canMatchEither() {
+
+        String result =
+                Match(Either.right(1)).of(Case($Left($()), "Left"), Case($Right($()), x -> x.toString()));
+
+        assertThat(result).isEqualTo("Right");
 
     }
 
     @Test
     public void canMatchLikeSwitch() {
+        Integer intValue = 1;
 
-        // Integer result = Match(Option.some(1)).of(Case($(1), 1), Case($(2), 2));
+        String result = Match(intValue).of(Case($(1), "1"), Case($(2), "2"), Case($(0), "-1"));
 
-        // assertThat(result).isEqualTo(1);
+        assertThat(result).isEqualTo("1");
 
     }
 
